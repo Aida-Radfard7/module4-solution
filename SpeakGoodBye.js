7fd1e4f77a0db9ec4e7f029966ff85348fbc9a84
Aida Radfard <aida.radfard.1@gmail.com>
2021-11-24 20:50:33 +0330

    first commit

---
+++ SpeakGoodBye.js
@@ -0,0 +1,10 @@
+
+(function sayBye(window){
+  var byeSpeaker = {}
+  var speakWord = "Good Bye";
+  byeSpeaker.speak = 
+    function speak(name) {
+      console.log(speakWord + " " + name);
+    }
+  window.byeSpeaker = byeSpeaker;
+})(window);
---
+++ SpeakHello.js
@@ -0,0 +1,10 @@
+
+(function sayHello(window){
+  var helloSpeaker = {}
+  var speakWord = "Hello";
+   helloSpeaker.speak = 
+    function speak(name) {
+      console.log(speakWord + " " + name);
+    }
+  window.helloSpeaker = helloSpeaker;
+})(window);
